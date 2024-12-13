156cc07b838442db996030a126267f287ee09463
Gianmarco G <giangarof98@gmail.com>
2024-12-13 15:37:14 -0500

    input

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ const key = '13e27bcd'
 
 const leftMovie = ref([])
 const rightMovie = ref([])
-
 const inputLeft = ref('')
+const inputRight = ref('')
 
 const fetch = async(s) => {
   try {
@@ -16,8 +16,6 @@ const fetch = async(s) => {
     })
     console.log(res.data)
     return res.data
-    // leftMovie.value = res.data
-    // rightMovie.value = res.data
   } catch (error) {
     
   }
@@ -26,11 +24,10 @@ const fetch = async(s) => {
 const handleSubmit = async(side, search) => {
   // console.log(search)
   if(side == 'left'){
-    // await fetch(search)
-    console.log(search)
-    leftMovie.value = await fetch(search)
+    leftMovie.value = await fetch(inputLeft.value)
+    // console.log(resLeft)
   } else if (side == 'right'){
-    fetch(rightMovie.value)
+    rightMovie.value = await fetch(inputRight.value)
   }
 }
 
@@ -43,23 +40,25 @@ const handleSubmit = async(side, search) => {
 
     <div class="flex flex-row justify-evenly">
 
-      <form @submit.prevent="handleSubmit('left', inputLeft)" class="flex">
+      <form @submit.prevent="handleSubmit('left')" class="flex">
         <button type="submit" class="text-gray-700 bg-green-300 p-2 rounded-l-md">Search</button>
         <input type="text" v-model="inputLeft" class="border-2 focus:outline-none">
       </form>
 
       <form @submit.prevent="handleSubmit('right')" class="flex">
-        <input type="text" v-model="rightMovie" class="border-2 focus:outline-none">
+        <input type="text" v-model="inputRight" class="border-2 focus:outline-none">
         <button type="submit" class="text-gray-700 bg-green-300 p-2 rounded-r-md">Search</button>
       </form>
     </div>
 
-    <p v-if="!leftMovie.length || !rightMovie.length">Start searching!</p>
+    <!-- <p v-if="!leftMovie.length || !rightMovie.length">Start searching!</p> -->
 
       <div v-if="leftMovie.length">
-        <div v-for="x in leftMovie" :key='x.imdbID'>
-          {{x}}
-        </div>
+        <ul>
+          <li v-for="x in leftMovie" :key='x.imdbID'>
+            <p>{{x.Title}}</p>
+          </li>
+        </ul>
       </div>
     <div>
 
